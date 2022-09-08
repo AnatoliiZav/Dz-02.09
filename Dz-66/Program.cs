3c1c43289c418e23c3abf2f73860483b35b14122
Anatolii <zavorinanatolii@mail.ru>
2022-09-08 16:47:22 +0800

    Dz-66

---
+++ Dz-66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+int Print(int N, int count)  
+{
+if (N == count) return N;
+return N + Print(N + 1, count);  
+}
+
+Console.Write("Введите первое число M = ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число N = ");
+int number = int.Parse(Console.ReadLine());
+
+PrintN(number1, number);
+Console.WriteLine();
+Console.WriteLine(Print(number1, number));
